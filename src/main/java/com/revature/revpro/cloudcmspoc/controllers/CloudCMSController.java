7e58a3969cbaa7e2777f4d326d2c8d081efc303f
RagavanRevature <ragavanpv@revature.com>
2020-03-04 22:47:21 +0530

    Changed the request method

--- src/main/java/com/revature/revpro/cloudcmspoc/controllers/CloudCMSController.java
+++ src/main/java/com/revature/revpro/cloudcmspoc/controllers/CloudCMSController.java
@@ -6,6 +6,8 @@ import org.gitana.platform.support.ResultMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,13 +33,12 @@ public class CloudCMSController {
      *
      * @return
      */
-    @PostMapping(value = "/repositories",
+    @GetMapping(value = "/repositories",
         produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResultMap<Repository>> getRepositories(
-        @RequestBody List<String> codes) {
+    public ResponseEntity<ResultMap<Repository>> getRepositories() {
         return Optional.ofNullable(cmsService.getRepositories())
-            .filter(Objects::nonNull)
-            .map(activityType ->ResponseEntity.ok(activityType))
+            .filter(repositoryResultMap->!CollectionUtils.isEmpty(repositoryResultMap))
+            .map(ResponseEntity::ok)
             .orElse(ResponseEntity.noContent().build());
     }
 
