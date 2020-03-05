a14b78de6ec2c6b49056e5637e50af0ac392c0cc
PavanSrivatsav <pavansrivatsav96@gmail.com>
2020-03-05 14:30:38 +0530

    fixed startup

--- src/test/java/com/revature/revpro/cloudcmspoc/CloudCmsPocApplicationTests.java
+++ src/test/java/com/revature/revpro/cloudcmspoc/CloudCmsPocApplicationTests.java
@@ -1,6 +1,6 @@
 package com.revature.revpro.cloudcmspoc;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
